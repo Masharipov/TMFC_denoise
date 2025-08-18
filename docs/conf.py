c17cff790fcd40ffb896326f5e397bb7409ecce1
Ruslan Masharipov <masharipov@ihb.spb.ru>
2025-08-18 23:25:55 +0300

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,19 @@
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'TMFC_denoise'
+copyright = '2025, Ruslan Masharipov'
+author = 'Ruslan Masharipov'
+release = '1.3'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.intersphinx"]
+
+templates_path = ["_templates"]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
